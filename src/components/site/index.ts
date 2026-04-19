b59c873b6daee78a282334d6dc4a883abaa6d07f
chuyuewei <3140194376@qq.com>
2026-04-19 22:55:32 +0800

    a

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,12 +1,13 @@
 // src/components/site/index.ts
 // 统一导出所有基础组件
 
-export { PageHeader }      from './PageHeader'
-export { SectionHeader }   from './SectionHeader'
-export { PageFooter }      from './PageFooter'
-export { ArchiveBlock }    from './ArchiveBlock'
-export { QuoteBlock }      from './QuoteBlock'
-export { WarningBox }      from './WarningBox'
+export { PageHeader }       from './PageHeader'
+export { SectionHeader }    from './SectionHeader'
+export { PageFooter }       from './PageFooter'
+export { ArchiveBlock }     from './ArchiveBlock'
+export { QuoteBlock }       from './QuoteBlock'
+export { WarningBox }       from './WarningBox'
 export { Badge, StatusDot } from './Badge'
-export { InfoPanel }       from './InfoPanel'
-export { RelatedBar }      from './RelatedBar'
+export { InfoPanel }        from './InfoPanel'
+export { RelatedBar }       from './RelatedBar'
+export { ClosingStatement } from './ClosingStatement'
